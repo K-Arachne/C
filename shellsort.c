b91fb264db37068d55d32157bee6a5ee9101c256
seisyo58 <seisyo58@gmail.com>
2017-10-26 14:15:42 +0900

    3rd

--- shellsort.c
+++ shellsort.c
@@ -3,18 +3,23 @@
 #define N 10
 
 int seiretu(int kumi,int *num[]){
-	int i;
+	int i,kari;
 	for(i=0;i<N/kumi;i++){
-					num
+		if(num[i]>num[i+kumi]){
+			kari=num[i];
+			num[i]=num[i+kumi];
+			num[i+kumi]=kari;
+		}
+	}
 }
 
-int main(void){
+	int main(void){
 		int i,num[N];
 
 		srand((unsigned)time(NULL));
 		for(i=0;i<N;i++){
-				num[i]=rand()%(N+1);
+			num[i]=rand()%(N+1);
 		}
 
 		int kumi=N/2
-}
+	}
