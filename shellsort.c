5e02f606c043db6a0a10165c65f28b485636786e
seisyo58 <seisyo58@gmail.com>
2017-10-26 13:54:45 +0900

    2nd

---
+++ shellsort.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<time.h>
+#define N 10
+
+int seiretu(int kumi,int *num[]){
+	int i;
+	for(i=0;i<N/kumi;i++){
+					num
+}
+
+int main(void){
+		int i,num[N];
+
+		srand((unsigned)time(NULL));
+		for(i=0;i<N;i++){
+				num[i]=rand()%(N+1);
+		}
+
+		int kumi=N/2
+}
