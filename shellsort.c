8fbf29da20e24e084214dae010ccf3c38e2a7819
seisyo58 <seisyo58@gmail.com>
2017-10-31 14:17:17 +0900

    完成版

--- shellsort.c
+++ shellsort.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<time.h>
-#define N 20
+#define N 20000
 
 void myPrint(int *);
 
@@ -19,41 +19,52 @@ void seiretu(int kumi,int *num){
 			}
 		}
 	}
-	printf("%d間隔でソート\n",kumi);
-	myPrint(num);
+	printf("\n間隔%dでソート\n",kumi);
+	//myPrint(num);
 }
 
 void myPrint(int *num){
 	int i;
 	for(i=0;i<N;i++){
-		printf("%2d ",num[i]);
+		printf("%d ",num[i]);
 	}
 	printf("\n");
 }
 
 int main(void){
-	int i,num[N];
+	int i,num[N],yes;
 
 	srand((unsigned)time(NULL));
 	for(i=0;i<N;i++){
-		//num[i]=rand()%(N+1);
-		num[i]=N-i;
+		num[i]=rand()%(N+1);
 	}
 
-	printf("元データ\n");
-	myPrint(num);
+	printf("元データ(%d個)を表示しますか(0 or 1)-> ",N);
+	scanf("%d",&yes);
+	if(yes){
+		myPrint(num);
+	}
 
 	int h=1;
 	while(h<=((N-9)/9)){
 		h=3*h + 1;
 	}
+
+	clock_t t_start,t_end;
+	t_start=clock();
 	while(h>0){
 		seiretu(h,num);
 		h=(h-1)/3;
 	}
+	t_end = clock();
 
-	printf("結果\n");
-	myPrint(num);
+	printf("\n%f秒でソートしました\n",(double)(t_end-t_start)/CLOCKS_PER_SEC);
 
+	printf("結果を表示しますか(0 or 1)-> ");
+	scanf("%d",&yes);
+	if(yes){
+		myPrint(num);
+	}
 
+	return 0;
 }
